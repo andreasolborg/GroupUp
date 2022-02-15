6d13fec9bafbe2d446608170ad65a8a1224bc06f
karansingh01 <karanss@stud.ntnu.no>
2022-02-15 09:08:21 +0100

    added files and folders for coding

--- group-up/src/App.js
+++ group-up/src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import { Switch, Route } from 'react-router-dom';
+import { Routes ,Route } from 'react-router-dom'
+//import { Switch } from 'react-router-dom'
 
-function App() {
+import './App.css';
+import login1 from './pages/login/login.jsx';
+
+
+const loginPage = () => (
+<div>
+<h1>please login</h1>
+</div>
+)
+
+const PantsPage = () => (
+    <div>
+    <h1> Pants PAGE</h1>
+    </div>
+  )
+
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      <h1>Meet new friends!!</h1>
-      
-      </header>
+      <Routes> 
+      <Route exact path='/pants' element={PantsPage} />
+      <Route path='/' element={login1} />
+      </Routes>
     </div>
   );
+}
 }
 
 export default App;
---
+++ group-up/src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './login.css';
+//import Directory from '../../components/directory/directory.component';
+
+const login = () => (
+<div className='login'>
+<h1>HEI HEI LOG INN </h1>
+</div>
+);
+
+export default login;
