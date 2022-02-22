3ae1158bdc2187c47eedcb07bc5ebee3d3fc83c4
Catarina Skibsrud <catarins@stud.ntnu.no>
2022-02-22 10:15:03 +0100

    added card component

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -3,22 +3,17 @@ import Navbar from "../../components/navbar";
 import "./homepage.css";
 import pic from "./Climbing.png"
 import { Button } from "@material-ui/core";
+import Card from "../../components/profileCard"
 
 const Homepage = () => {
     return (
         <div className="homepage">
             <Navbar className="navbar"></Navbar>
-            <div className="group">
-                    <h1>Interest</h1>
-                    <img source={pic} alt="pic"/>
-                    <Button className="button" variant="outlined">Match</Button>
-                    <div className="info">
-                        <p>Interest: </p>
-                        <p>Age: </p>
-                        <p>Meeting time: </p>
-                    </div>
+            <div className="cardGroup">
+                <Card className="profileCard"></Card>
             </div>
         </div>
+        
     );
 };
 export default Homepage;
