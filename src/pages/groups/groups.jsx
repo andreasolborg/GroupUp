9984c3390bd64e6b7678569ecfa41f499e26a54e
masun <masun@ntnu.stud.no>
2022-03-10 18:01:29 +0100

    Added time filter
    a user can now filter groups based on a time interval (as well as interest and location)

--- src/pages/groups/groups.jsx
+++ src/pages/groups/groups.jsx
@@ -10,6 +10,9 @@ import { CardList } from "./cardlist";
 import "./card.css";
 import Button from '@material-ui/core/Button';
 import Navbar from "../../components/navbar";
+import DateTimePicker from 'react-datetime-picker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 
 
@@ -17,6 +20,11 @@ export default function Groups() {
 
     const [groups, setGroups] = useState([]);
     const [groupTemp, setGroupTemp] = useState([]);
+
+    const [startTime, setStartTime] = useState(new Date());
+    const [endTime, setEndTime] = useState(new Date());
+    const [timeToggled, setTimeToggled] = useState(false);
+
     const groupsCollectionReference = collection(db, "groups");
     const navi = useNavigate();
 
@@ -24,14 +32,14 @@ export default function Groups() {
         navi("/user");
     }
 
-  
+
 
     useEffect(() => {
         const getGroups = async () => {
             const querySnapshot = await getDocs(groupsCollectionReference);
-            setGroups(querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id})));
-            setGroupTemp(querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id})));
-        
+            setGroups(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            setGroupTemp(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
         };
         getGroups();
     }, []);
@@ -40,19 +48,80 @@ export default function Groups() {
         var arrRes = [];
         groups.forEach((g) => {
             if (g.interest.toLowerCase().includes(document.getElementById("searchInput").value.toLowerCase()) &&
-            g.location.toLowerCase().includes(document.getElementById("locationSearchInput").value)){
+                g.location.toLowerCase().includes(document.getElementById("locationSearchInput").value)) {
+                if (timeToggled) {
+                    if (new Date(g.datetime.seconds * 1000) > startTime && new Date(g.datetime.seconds * 1000) < endTime) {
+                        arrRes.push(g);
+                    }
+                } else {
+                    arrRes.push(g);
+                }
+            }
+        });
+        setGroupTemp(arrRes);
+    }
+
+    const filterByTime = () => {
+        setTimeToggled(true);
+        var arrRes = [];
+
+        groupTemp.forEach((g) => {
+            if (new Date(g.datetime.seconds * 1000) > startTime && new Date(g.datetime.seconds * 1000) < endTime) {
+                console.log("Yurr");
                 arrRes.push(g);
             }
         });
-        setGroupTemp(arrRes);  
+        setGroupTemp(arrRes);
+        document.getElementById("filterByTimeButton").style = "color: green";
     }
-   
+
+    const cancelTimeFilter = () => {
+        setTimeToggled(false);
+        var arrRes = [];
+        groups.forEach((g) => {
+            if (g.interest.toLowerCase().includes(document.getElementById("searchInput").value.toLowerCase()) &&
+                g.location.toLowerCase().includes(document.getElementById("locationSearchInput").value)) {
+                arrRes.push(g);
+            }
+        });
+        setGroupTemp(arrRes);
+        document.getElementById("filterByTimeButton").style = "color: black";
+    }
+
+    const delay = (time) => {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+
 
     return <div className="topOfGroups">
         <Navbar></Navbar>
         <h1>GROUPS PAGE</h1>
-        <input id="searchInput" placeholder="search by interest..." onChange={() => {searchBarChanged()}}/>
-        <input id="locationSearchInput" placeholder="search by location..." onChange={() => {searchBarChanged()}}/>
-        <CardList groups={groupTemp}/>
+        <input id="searchInput" placeholder="search by interest..." onChange={() => { searchBarChanged() }} />
+        <input id="locationSearchInput" placeholder="search by location..." onChange={() => { searchBarChanged() }} />
+        <div>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                    renderInput={(props) => <TextField {...props} />}
+                    label="DateTimePicker"
+                    value={startTime}
+                    onChange={(newValue) => {
+                        setStartTime(newValue);
+                    }}
+                />
+            </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                    renderInput={(props) => <TextField {...props} />}
+                    label="DateTimePicker"
+                    value={endTime}
+                    onChange={(newValue) => {
+                        setEndTime(newValue);
+                    }}
+                />
+            </LocalizationProvider>
+            <button id="filterByTimeButton" onClick={filterByTime}>Apply Time Filter</button>
+            <button onClick={() => cancelTimeFilter()}>Remove time filter</button>
+        </div>
+        <CardList groups={groupTemp} />
     </div>
 }
