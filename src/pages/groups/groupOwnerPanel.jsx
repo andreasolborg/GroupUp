e9061f06ad407ff6e239278114e3e64c5cf88688
Ingeborg Enes Kjevik <ingeboek@stud.ntnu.no>
2022-03-17 11:21:46 +0100

    changed design on admin panel

--- src/pages/groups/groupOwnerPanel.jsx
+++ src/pages/groups/groupOwnerPanel.jsx
@@ -1,49 +1,55 @@
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import { Button, TextareaAutosize, TextField, Box, IconButton, InputAdornment} from '@mui/material';
+import { Button, TextareaAutosize, TextField, Box, IconButton, InputAdornment } from '@mui/material';
 import React from 'react';
 //import DateTimePicker from 'react-datetime-picker';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import { Grid } from '@mui/material';
 
 
-export default function GroupOwnerPanel({ 
-    hideAdminButton, 
-    updateGroupDetails, 
-    removeUserButton, 
-    setNewDate, 
-    sendNewDescription, 
-    requests, 
+export default function GroupOwnerPanel({
+    hideAdminButton,
+    updateGroupDetails,
+    removeUserButton,
+    setNewDate,
+    sendNewDescription,
+    requests,
     leaveGroup,
     addUserButton,
     setDateTime,
-    dateTime, 
-    acceptRequestButton }) 
-    {
+    dateTime,
+    acceptRequestButton }) {
 
     return (
         
-        <div id="admin">
+        <div id="admin" >
             
             <div className="text">
-                <Button variant="contained" onClick={hideAdminButton} style={{backgroundColor:'#DDEEF5', color:'black'}}>Hide Admin Priviliges</Button>
+                <Button id="btnID" variant="contained" onClick={hideAdminButton} >Hide Admin Priviliges</Button>
+                <Button id="btnID" variant="contained" className="obsButton" onClick={leaveGroup} >Delete Group</Button>
                 <h2>Gruppeleder</h2>
                 <p>These functions are hidden for regular members</p>
-
+            </div>
+            <Grid container>
+                <Grid xs={3}>
                 <div className="update-details">
                     <h3>Update Group Details</h3>
-                    <TextField placeholder="Enter a group name" id="groupNameInput" variant='outlined' style={{backgroundColor:"white"}}/>
-                    <TextField placeholder="Enter a new interest" id="interestInput" style={{backgroundColor:"white"}}/>
-                    <TextField placeholder="Enter a new location" id="locationInput" style={{backgroundColor:"white"}}/>
+                    <TextField placeholder="Enter a group name" id="groupNameInput" variant='standard' />
+                    <br/>
+                    <TextField placeholder="Enter a new interest" id="interestInput" variant='standard' />
+                    <br/>
+                    <TextField placeholder="Enter a new location" id="locationInput" variant='standard'/>
                 </div>
-                <Button variant="contained" onClick={updateGroupDetails} style={{backgroundColor:'#DDEEF5', color:'black'}}>Send</Button>
+               
+                <Button id="btnID" variant="contained" onClick={updateGroupDetails} >Send</Button>
+            </Grid>
 
-
-            </div>
-            <div className="remove-users">
+            <Grid xs={3}>
+             <div className="remove-users">
                 <h3>Remove users</h3>
-                <TextField placeholder="Enter user-mail" id="removeUserInput" style={{backgroundColor:"white"}}
+                <TextField variant='standard' placeholder="Enter user-mail" id="removeUserInput"
                     InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
@@ -55,9 +61,11 @@ export default function GroupOwnerPanel({
                       }}
                 />
             </div>
+            </Grid>
+            <Grid xs={3}>
             <div className="remove-users">
                 <h3>Add users</h3>
-                <TextField placeholder="Enter user-mail" id="addUserInput" style={{backgroundColor:"white"}} 
+                <TextField  variant='standard' placeholder="Enter user-mail" id="addUserInput"  
                     InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
@@ -68,10 +76,9 @@ export default function GroupOwnerPanel({
                         ),
                       }}
                 />
-                   
-                
-               
             </div>
+            </Grid>
+            <Grid xs={3}>
             <div>
                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                     <DateTimePicker
@@ -85,27 +92,32 @@ export default function GroupOwnerPanel({
                     />
                 </LocalizationProvider>
             </div>
-                <Button variant='contained' onClick={setNewDate} style={{backgroundColor:'#DDEEF5', color:'black'}}>Send</Button>
-            
+               
+                <Button id="btnID" variant='contained' onClick={setNewDate} >Send</Button>
+            </Grid>
 
+            <Grid xs={6}  >
             <div id="des-container">
                 <TextareaAutosize id="des" minRows="5" placeholder="Enter new description" style={{width: 200, backgroundColor:'white'}}/>
-                <Button variant='contained' onClick={sendNewDescription} style={{backgroundColor:'#DDEEF5', color:'black'}}>Submit description</Button>
+                <br/>
+                <Button id="btnID" variant='contained' onClick={sendNewDescription} >Submit description</Button>
             </div>
-
+            </Grid>
+            <Grid xs={6} direction="row">
             <div className="text">
                 <h2>The request queue</h2>
                 {requests.map((r) => (
                     <div className="membersList">
                         <div className="text">
                             <h3>{r}</h3>
-                            <Button variant='contained' onClick={() => acceptRequestButton(r)} style={{backgroundColor:'#DDEEF5', color:'black'}}>Accept request</Button>
+                            <Button id="btnID" variant='contained' onClick={() => acceptRequestButton(r)} >Accept request</Button>
                         </div>
                     </div>
                 ))}
             </div>
-            <Button variant="contained" className="obsButton" onClick={leaveGroup} style={{backgroundColor:'#DDEEF5', color:'black'}}>Delete Group</Button>
-            </div> 
+            </Grid>
+        </Grid>
+        </div>
        
         
         
