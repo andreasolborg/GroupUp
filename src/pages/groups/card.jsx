7763ce1d20eabc3a963a776c20689cb93a5662c4
Lars-over <larsover@ntnu.no>
2022-03-29 08:01:25 +0200

    Started trying to fix card styling bug

--- src/pages/groups/card.jsx
+++ src/pages/groups/card.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { auth } from "../../firebase-config";
 import { db } from "../../firestore";
-import { collection, arrayRemove, getDocs, addDoc, updateDoc, doc, deleteDoc, getDoc, setDoc, getDocFromServer, query, where, arrayUnion } from 'firebase/firestore'
-import { CircularProgress, getBottomNavigationUtilityClass } from "@mui/material";
-import { CardList } from "./cardlist";
+import { updateDoc, doc, arrayUnion } from 'firebase/firestore'
+import { CircularProgress } from "@mui/material";
 import Button from '@material-ui/core/Button';
 import "./card.css";
 import { makeStyles } from "@material-ui/core";
 import PopUp from "../../components/popup";
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@mui/material/Grid';
 import { storage } from "../../firebase-config";
-import { ref, uploadBytes, getDownloadURL, getStorage, deleteObject } from "firebase/storage";
+import { ref, getDownloadURL } from "firebase/storage";
 
 export const MediaCard = (props) => {
-    const [reloader, setReloader] = useState([]);
     const navi = useNavigate();
     const [feedbackMessage, setFeedbackMessage] = useState("");
     const [severity, setSeverity] = useState("");
@@ -29,9 +25,6 @@ export const MediaCard = (props) => {
 
     //Popup
     const [open, setOpen] = useState(false);
-    const handleClick = () => {
-        setOpen(true);
-    };
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
@@ -86,27 +79,12 @@ export const MediaCard = (props) => {
     }
 
     const useStyles = makeStyles({
-        gridContainer: {
-            paddingTop: "10px",
-            width: "700px",
-            direction: "column",
-            alignItems: "stretch",
-            display: "inline-block",
-            justifyContent: "center",
-            xs: 12,
-            md: 6,
-            lg: 4,
-            marginTop: "auto"
-        },
         root: {
             textAlign: "center"
         },
         media: {
             height: 140
         },
-        content: {
-
-        }
     })
 
     const classes = useStyles();
@@ -153,39 +131,30 @@ export const MediaCard = (props) => {
         });
     }
 
-
-
     return (
-        <div>
-            <Card className={classes.root}>
-                <CardActionArea>
-                    {
-                        url ? <CardMedia className={classes.media} image={url} /> : <CircularProgress />
-                    }
-                    <CardContent className={classes.content}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {props.group.groupName}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {props.group.interest}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {props.group.location}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {new Date(props.group.datetime.seconds * 1000).toUTCString()}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions className={classes.button}>
-                    <div className="card-buttons">
-                        <Button id="visitGroupButton" onClick={() => { enterGroup(props.group.id) }} variant="outlined">Visit group</Button>
-                        <Button id="requestButton" onClick={() => { requestToJoin() }} variant="outlined">Request to join group</Button>
-                        <PopUp open={open} severity={severity} feedbackMessage={feedbackMessage} handleClose={handleClose}>
-                        </PopUp>
-                    </div>
-                </CardActions>
-            </Card>
-        </div>
+        <Card className={classes.root}>
+            {
+                url ? <CardMedia className={classes.media} image={url} /> : <CircularProgress />
+            }
+            <CardContent >
+                <Typography gutterBottom variant="h5" component="h2">
+                    {props.group.groupName}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {props.group.interest}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {props.group.location}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {new Date(props.group.datetime.seconds * 1000).toUTCString()}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button id="visitGroupButton" onClick={() => { enterGroup(props.group.id) }} variant="outlined">Visit group</Button>
+                <Button id="requestButton" onClick={() => { requestToJoin() }} variant="outlined">Request to join group</Button>
+            </CardActions>
+            <PopUp open={open} severity={severity} feedbackMessage={feedbackMessage} handleClose={handleClose} />
+        </Card>
     );
 }
